f5f5c434d66661c6bd0691a4703b9b66839c1a68
esyef <yeferson.tech@gmail.com>
2024-02-14 16:01:22 -0500

    Add: Status field, page 18

--- Chap01/mysite/blog/models.py
+++ Chap01/mysite/blog/models.py
@@ -3,12 +3,21 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+
+    # Status field
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Publised'
+
+    
     title = models.CharField(max_lengh=250)
     slug = models.SlugField(max_lenght=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.TextChoices(max_lenght= 2, choices=Status.choices, default=Status.DRAFT)
+
 
     # class to sorted Post
     # this class define metadata for the model
