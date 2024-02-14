d04ca383f2b62fa382df33df6a14be72ca4440ca
esyef <yeferson.tech@gmail.com>
2024-02-14 13:41:58 -0500

    Add: Meta class,  controller metadata Post model

--- Chap01/mysite/blog/models.py
+++ Chap01/mysite/blog/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_lengh=250)
     slug = models.SlugField(max_lenght=250)
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now=True)
 
+    # class to sorted Post
+    # this class define metadata for the model
+    class Meta:
+        ordering = ['-publish']
+        # index database improve performance for quries filtering
+        # or ordering results by this field
+        index = [
+            models.Index(fields=['-publish'])
+        ]
+
+    # return str to human redeable
     def __str__(self):
         return self.title
