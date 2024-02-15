bf38f401647d6be9b9db5389cd830ef92d0a7ecf
esyef <yeferson.tech@gmail.com>
2024-02-15 14:59:49 -0500

    feacture: add shorcut for HTTP 404 error

--- Chap01/mysite/blog/views.py
+++ Chap01/mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+# from django.http import Http404
 from .models import Post
 
 # Create your views here.
@@ -10,3 +11,15 @@ def post_list(request):
     return render(request, 
                   'blog/post/list.html', 
                   {'posts': posts})
+
+def post_detail(request, id):
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404('No post found.')
+
+    post = get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+    
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
