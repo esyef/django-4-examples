c50eaced58e926a0c1d3167f401a52a9c7f763dd
esyef <yeferson.dev@gmail.com>
2024-02-18 10:57:09 -0500

    feacture: Page exeption list post

--- Chap02/mysite/blog/views.py
+++ Chap02/mysite/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 # from django.http import Http404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 
 # Create your views here.
 
@@ -12,7 +12,15 @@ def post_list(request):
     # Pagination with 3 post per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
 
     return render(request, 
                   'blog/post/list.html', 
