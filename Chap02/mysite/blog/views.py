add5f9af181b8bb8e9e4100f51cb9a8012b089df
esyef <yeferson.dev@gmail.com>
2024-02-27 20:12:25 -0500

    add: class-based views

--- Chap02/mysite/blog/views.py
+++ Chap02/mysite/blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 # from django.http import Http404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
+# from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 
 # Create your views here.
 
 
+"""
 def post_list(request):
     post_list = Post.published.all()
 
@@ -25,6 +27,19 @@ def post_list(request):
     return render(request, 
                   'blog/post/list.html', 
                   {'posts': posts})
+"""
+
+class PostListView(ListView):
+    """
+        Alternative post list view
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
+
 
 def post_detail(request, year, month,day,post):
     # try:
@@ -45,3 +60,5 @@ def post_detail(request, year, month,day,post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
