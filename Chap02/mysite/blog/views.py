efe2563a87a473057ad9354903719ff95c7b29e5
esyef <yeferson.tech@gmail.com>
2024-03-01 15:57:54 -0500

    add: form to share post

--- Chap02/mysite/blog/views.py
+++ Chap02/mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 # from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 
 # Create your views here.
@@ -65,14 +66,24 @@ def post_detail(request, year, month,day,post):
 def post_share(request, post_id):
     # retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
     if request.method == 'POST':
         # for was submited
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # form fields passed validation
             cd = form.cleaned_data
+            post_url = request.build_absolute_url(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read"\
+                    f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"\
+                    f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject,message, 'account@mail.com', [cd['to']])
+            sent = True
+
             # ... send email
     else:
         form = EmailPostForm()
-    
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
